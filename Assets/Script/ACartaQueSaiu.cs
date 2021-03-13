dc28395d6b4063d5aed2d593d876e790b55acd40
MarianaCidade <79975190+MarianaCidade@users.noreply.github.com>
2021-03-13 17:51:15 +0000

    Update ACartaQueSaiuFoi

--- Assets/Script/ACartaQueSaiu.cs
+++ Assets/Script/ACartaQueSaiu.cs
@@ -12,7 +12,7 @@ public class ACartaQueSaiu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Olá, clica no espaço para eu te sortear uma carta");//escrever mensagem 
+        Debug.Log("Olá, clica no espaço para eu te sortear uma carta");
     }
 
     // Update is called once per frame
@@ -27,45 +27,46 @@ public class ACartaQueSaiu : MonoBehaviour
            
 
             if (numeroDoNaipe == 1)
+
             {
                 nomeDoNaipe = "Espadas";
-
             }
+
 
             if (numeroDoNaipe == 2)
+
             {
                 nomeDoNaipe = "Ouros";
-
             }
+
 
             if (numeroDoNaipe == 3)
+
             {
                 nomeDoNaipe = "Paus";
-
-
-
             }
+
 
             if (numeroDoNaipe == 4)
+
             {
                 nomeDoNaipe = "Copas";
-
-
-
             }
+
 
             if (numeroDaCarta == 11)
+
             {
                 nomeDaFigura = "Dama";
-
             }
+
 
             if (numeroDaCarta == 12)
 
             {
                 nomeDaFigura = "Valete";
-
             }
+
 
             if (numeroDaCarta == 13)
 
@@ -73,22 +74,24 @@ public class ACartaQueSaiu : MonoBehaviour
                 nomeDaFigura = "Rei";
             }
 
+
             if (numeroDaCarta == 14)
+
             {
                 nomeDaFigura = "Ás";
             }
 
 
             if (numeroDaCarta <= 10)
+
             {
                 Debug.Log("A carta que saiu é " + numeroDaCarta + " de " + nomeDoNaipe);
             }
 
             if (numeroDaCarta > 10)
+
             {
-
                 Debug.Log("A carta que saiu é " + nomeDaFigura + " de " + nomeDoNaipe);
-
             }
         }
 
