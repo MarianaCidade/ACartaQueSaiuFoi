0206139b6e2772e240eb2e92a344c1647d68d764
MarianaCidade <79975190+MarianaCidade@users.noreply.github.com>
2021-03-13 17:37:47 +0000

    Update ACartaQueSaiu

--- Assets/Script/ACartaQueSaiu.cs
+++ Assets/Script/ACartaQueSaiu.cs
@@ -4,15 +4,94 @@ using UnityEngine;
 
 public class ACartaQueSaiu : MonoBehaviour
 {
+    int numeroDaCarta;
+    int numeroDoNaipe;
+    string nomeDoNaipe;
+    string nomeDaFigura;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Olá, clica no espaço para eu te sortear uma carta");//escrever mensagem 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+      if (Input.GetKeyDown(KeyCode.Space))
+        { numeroDaCarta = Random.Range(1, 15);
+            numeroDoNaipe = Random.Range(1, 5);
+
+
+           
+
+            if (numeroDoNaipe == 1)
+            {
+                nomeDoNaipe = "Espadas";
+
+            }
+
+            if (numeroDoNaipe == 2)
+            {
+                nomeDoNaipe = "Ouros";
+
+            }
+
+            if (numeroDoNaipe == 3)
+            {
+                nomeDoNaipe = "Paus";
+
+
+
+            }
+
+            if (numeroDoNaipe == 4)
+            {
+                nomeDoNaipe = "Copas";
+
+
+
+            }
+
+            if (numeroDaCarta == 11)
+            {
+                nomeDaFigura = "Dama";
+
+            }
+
+            if (numeroDaCarta == 12)
+
+            {
+                nomeDaFigura = "Valete";
+
+            }
+
+            if (numeroDaCarta == 13)
+
+            {
+                nomeDaFigura = "Rei";
+            }
+
+            if (numeroDaCarta == 14)
+            {
+                nomeDaFigura = "Ás";
+            }
+
+
+            if (numeroDaCarta <= 10)
+            {
+                Debug.Log("A carta que saiu é " + numeroDaCarta + " de " + nomeDoNaipe);
+            }
+
+            if (numeroDaCarta > 10)
+            {
+
+                Debug.Log("A carta que saiu é " + nomeDaFigura + " de " + nomeDoNaipe);
+
+            }
+        }
+
+
     }
 }
